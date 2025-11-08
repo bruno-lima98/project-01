f79c747665d36239048281e04fa5fdf48100d9d9
Bruno Lima <brunolimawp@gmail.com>
2025-11-08 14:23:12 +0000

    DEPLOY FILES

--- project-1/predict.py
+++ project-1/predict.py
@@ -1,33 +1,42 @@
 import pandas as pd
 import os
 import pickle
-from preprocessing import preprocess_data
-from model_wrapper import StartupFailureModel
 from flask import Flask, request, jsonify
+from preprocessing import preprocess_data
 
-model_path = os.path.join(os.path.dirname(__file__), "model_C=0.1.bin")
+model_file = os.path.join(os.path.dirname(__file__), "model_C=0.1.bin")
 
-with open(model_path, "rb") as f_in:
-    model_wrapper = pickle.load(f_in)
+with open(model_file, "rb") as f_in:
+    dv, scaler, model = pickle.load(f_in)
 
 app = Flask('startup_failure')
+
 @app.route('/predict', methods=['POST'])
-
 def predict():
-    startup = request.get_json()
+    try:
+        startup = request.get_json()
+        if not startup:
+            return jsonify({'error': 'Empty or invalid JSON input'}), 400
 
-    df_startup = pd.DataFrame([startup])
+        df_startup = pd.DataFrame([startup])
+        df_proc, categorical, numerical = preprocess_data(df_startup)
 
-    df_processed, cat_cols, num_cols = preprocess_data(df_startup)
-    pred_class = model_wrapper.predict(df_processed)
-    pred_proba = model_wrapper.predict_proba(df_processed)
+        df_proc[numerical] = scaler.transform(df_proc[numerical])
+        X = dv.transform(df_proc.to_dict(orient='records'))
 
-    result = {
-        'failure_probability' : float(pred_proba),
-        'failure' : bool(pred_class)
-    }
+        pred_proba = model.predict_proba(X)[0, 1]
+        pred_class = model.predict(X)[0]
 
-    return jsonify(result)
+        result = {
+            'failure_probability': float(pred_proba),
+            'failure': bool(pred_class)
+        }
+
+        return jsonify(result)
+
+    except Exception as e:
+        print("❌ Erro durante a predição:", str(e))
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9696)
